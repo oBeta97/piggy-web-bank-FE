42e1cc5571d678cef88a29a17398a6013df66e54
oBeta97 <paolopellizzari97@gmail.com>
2024-12-10 17:10:07 +0100

    minor style changes

--- src/components/BaseComponents/BaseBox.tsx
+++ src/components/BaseComponents/BaseBox.tsx
@@ -18,9 +18,11 @@ const BaseBox = (props: IContentBoxProps) => {
                 border: '1px solid ' + PRIMARY_COLOR,
                 borderRadius: '25px',
                 maxWidth: '650px',
+                minWidth: '85%',
                 background: 'linear-gradient(135deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2))',
                 backdropFilter: 'blur(10px)',
                 boxShadow: 'inset 0 0 10px rgba(0, 0, 0, 0.2)',
+                padding:'1em'
             }}
         >
             {
--- src/components/HomePage/HomePageContent.tsx
+++ src/components/HomePage/HomePageContent.tsx
@@ -6,22 +6,22 @@ import TransactionByCategoryTable from "./TransactionByCategoryTable";
 
 const HomePageContent = () => {
     return (
-        <Grid2 container sx={{ width: '100%', height: '90%' }} spacing={2}>
-            <Grid2 size={4} sx={{ display: { xs: 'none', sm: 'block' } }}>
-                <Stack>
+        <Grid2 container sx={{ width: { xs:'98%', sm:'100%'}, height: '90%' }} justifyContent={"space-evenly"} >
+            <Grid2 size={4} sx={{ display: { xs: 'none', lg: 'block' } }} container alignItems={"center"} justifyContent={"center"}>
+                <Stack height='100%' alignItems={'center'} justifyContent={'center'}>
                     <BaseBox
                         title="Expenses by category"
                         content={<TransactionByCategoryTable />}
                     />
                 </Stack>
             </Grid2>
-            <Grid2 size={{ xs: 12, sm: 4 }}>
+            <Grid2 size={{ xs: 12, lg: 4 }} container alignItems={"center"} justifyContent={"center"}>
                 <BaseBox
                     title="Transactions"
                     content={<VariableTransactionsTable />}
                 />
             </Grid2>
-            <Grid2 size={4} sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <Grid2 size={4} sx={{ display: { xs: 'none', lg: 'block' } }} container alignItems={"center"} justifyContent={"center"}>
                 cioafaiuerfh
             </Grid2>
         </Grid2>
